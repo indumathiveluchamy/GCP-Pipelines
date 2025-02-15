342e9c56b19d88252e1a3d6b62aecf4c15c71e62
indumathiveluchamy <133809287+indumathiveluchamy@users.noreply.github.com>
2025-02-14 19:51:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import os  # Import the 'os' module
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "indumathi"  
+    return "Assignment 2 - YourStudentID"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    print("Starting the Flask server...")  # Add this line
+    port = int(os.environ.get('PORT', 8080)) #Get port from environment and default to 8080 if not set.
+    app.run(debug=True, host='0.0.0.0', port=port)
