6ae1bfb6a1ddfed9cb53e170172172817ba6b0c5
indumathiveluchamy <133809287+indumathiveluchamy@users.noreply.github.com>
2025-02-14 20:49:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Assignment 2 - YourStudentID"
+    return "indumathi"
 
 if __name__ == '__main__':
     print("Starting the Flask server...")  # Add this line
     port = int(os.environ.get('PORT', 8080)) #Get port from environment and default to 8080 if not set.
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=8080)
